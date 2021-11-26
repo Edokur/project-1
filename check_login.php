fb9a0aa602bf44c993dd08eaf33d8224364efab3
EdoKur <edok7179@gmail.com>
2021-11-27 02:01:06 +0700

    membenarkan file check login

--- check_login.php
+++ check_login.php
@@ -2,23 +2,23 @@
 include "connection_database.php";
 $emailUser = $_POST['email_user'];
 $passwordUser = md5($_POST['pasword_user']);
-$sql="SELECT * FROM users WHERE id_user='$id_user' AND password='$passwordUser'";
+$sql="SELECT * FROM users WHERE email='$emailUser' AND password='$passwordUser'";
 $login=mysqli_query($con,$sql);
 $ketemu=mysqli_num_rows($login);
 $r= mysqli_fetch_array($login);
 
 if ($ketemu > 0){
     session_start();
-    $_SESSION['iduser'] = $r['id_user'];
-    $_SESSION['passuser'] = $r['password'];
+    $_SESSION['emailUser'] = $r['email_user'];
+    $_SESSION['passUser'] = $r['password_user'];
     echo"USER BERHASIL LOGIN<br>";
-    echo "id user =",$_SESSION['iduser'],"<br>";
-    echo "password=",$_SESSION['passuser'],"<br>";
+    echo "email User =",$_SESSION['emailUser'],"<br>";
+    echo "password User=",$_SESSION['passUser'],"<br>";
     echo "<a href=logout.php><b>LOGOUT</b></a></center>";
 }
 else{
     echo "<center>Login gagal! username & password tidak benar<br>";
-    echo "<a href=form_login.php><b>ULANGILAGI</b></a></center>";
+    echo "<a href=index.php><b>ULANGILAGI</b></a></center>";
 }
 mysqli_close($con);
 ?>
