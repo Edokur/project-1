19e786b55d5c2cff73a71aa3bd95ce2fca1c5dfe
EdoKur <edok7179@gmail.com>
2021-11-27 02:12:36 +0700

    Halaman Login user telah berhasil

--- Proses Login/check_login.php
+++ Proses Login/check_login.php
@@ -1,20 +1,14 @@
 <?php
 include "connection_database.php";
 $emailUser = $_POST['email_user'];
-$passwordUser = md5($_POST['pasword_user']);
+$passwordUser = md5($_POST['password_user']);
 $sql="SELECT * FROM users WHERE email='$emailUser' AND password='$passwordUser'";
 $login=mysqli_query($con,$sql);
 $ketemu=mysqli_num_rows($login);
 $r= mysqli_fetch_array($login);
 
 if ($ketemu > 0){
-    session_start();
-    $_SESSION['emailUser'] = $r['email_user'];
-    $_SESSION['passUser'] = $r['password_user'];
-    echo"USER BERHASIL LOGIN<br>";
-    echo "email User =",$_SESSION['emailUser'],"<br>";
-    echo "password User=",$_SESSION['passUser'],"<br>";
-    echo "<a href=logout.php><b>LOGOUT</b></a></center>";
+    header("Location : ../dashboard.php");
 }
 else{
     echo "<center>Login gagal! username & password tidak benar<br>";
