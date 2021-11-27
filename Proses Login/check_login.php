445c74801c4f2ec4c928c26cf8007af52b5528e4
Ajibon-cmd <teguhbayu52@gmail.com>
2021-11-27 22:04:11 +0700

    update check login

--- Proses Login/check_login.php
+++ Proses Login/check_login.php
@@ -1,24 +1,29 @@
 <?php
+session_start();
 include "connection_database.php";
 $emailUser = $_POST['email_user'];
 $passwordUser = md5($_POST['password_user']);
+$captcha = md5($_POST['captcha_code']);
 $sql="SELECT * FROM users WHERE email='$emailUser' AND password='$passwordUser'";
-if (isset($_POST['submit'])) {
-    $login=mysqli_query($con,$sql);
-    $ketemu=mysqli_num_rows($login);
-    $r= mysqli_fetch_array($login);
-    
-    if ($ketemu > 0){
-        session_start();
-        $_SESSION['emailUser'] = $emailUser;
-        $_SESSION['status'] = "login";
-        echo"USER BERHASIL LOGIN<br>";
-        header("location:../dashboard.php");
+$sql2="UPDATE users SET captcha ='$captcha' where email ='$emailUser'";
+
+    if ($_POST["captcha_code"] == $_SESSION["captcha_code"]){
+        $update=mysqli_query($con, $sql2);
+        $login=mysqli_query($con,$sql);
+        $ketemu=mysqli_num_rows($login);
+        $r= mysqli_fetch_array($login);
+        
+        if ($ketemu > 0){
+            session_start();
+            $_SESSION['emailUser'] = $emailUser;
+            $_SESSION['status'] = "login";
+            echo"USER BERHASIL LOGIN<br>";
+            header("location:../dashboard.php");
+        }
+        else{
+            echo "<center>Login gagal! username & password tidak benar<br>";
+            echo "<a href=index.php><b>ULANGILAGI</b></a></center>";
+        }
+        mysqli_close($con);
     }
-    else{
-        echo "<center>Login gagal! username & password tidak benar<br>";
-        echo "<a href=index.php><b>ULANGILAGI</b></a></center>";
-    }
-    mysqli_close($con);
-}
 ?>
