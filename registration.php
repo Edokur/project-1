b3aad2fb1e7442561d84bfee5590c8ecaefba345
EdoKur <edok7179@gmail.com>
2021-11-28 06:17:46 +0700

    validasi pendaftaran

--- registration.php
+++ registration.php
@@ -9,18 +9,82 @@
         <link rel="stylesheet" href="style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
 
-    <?php
-        require "Proses Login/connection_database.php";
-        if (isset($_POST["register"])) {
-            if (registrasi($_POST) > 0) {
-                echo "<script>
-                alert('Data Berhasil Ditambahkan');
-                </script>";
-            } else {
-                echo mysqli_error($con);
+        <?php
+// define variables and set to empty values
+$namaErr = $userErr = $emailErr = $passErr = $pass2Err = $captErr = "";
+$nama = $user = $email = $pass = $pass2 = $capt = "";
+
+//Digunakan untuk  memvalidasi semua data supaya data tidak kosong
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["nama_user"]) | empty($_POST["username_user"]) | empty($_POST["email_user"]) | empty($_POST["password_user"]) | empty($_POST["password_user2"]) | (empty($_POST["captcha_code"]))) 
+    {
+        if (empty($_POST["nama_user"])) {        //digunakan untuk memvalidasi nama supaya tidak kosong
+            $namaErr = "Nama harus diisi";
+        }else {
+            $nama = test_input($_POST["nama_user"]);
+        }
+    
+        if (empty($_POST["username_user"])) {        //digunakan untuk memvalidasi nama supaya tidak kosong
+            $userErr = "username harus diisi";
+        }else {
+            $user = test_input($_POST["username_user"]);
+        }
+    
+        if (empty($_POST["email_user"])) {   //digunakan untuk mengecek email supaya tidak kosong 
+            $emailErr = "Email harus diisi";
+        }else {
+            $email = test_input($_POST["email_user"]);
+    
+        // check if e-mail address is well-formed
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailErr = "Email tidak sesuai format"; 
             }
         }
-    ?>
+    
+        if (empty($_POST["password_user"])) {     //mengecek supaya website user tidak kosong
+            $passErr = "Password tidak boleh kosong";
+        }else {
+            $pass = md5($_POST["password_user"]);
+        }
+        
+        if (empty($_POST["password_user2"])) {     //mengecek supaya komentar tidak kosong
+            if ($pass2 !== $pass) {
+                $pass2Err = "Password tidak sama";
+            }
+        }else {
+            $pass2 = test_input($_POST["password_user2"]);
+        }
+        
+        if (empty($_POST["captcha_code"])) {      //mengecek supaya gender wajib dipilih salah satu
+            $captErr = "Harus di isi";
+        }else {
+            $capt = md5($_POST["captcha_code"]);
+        }
+        include "Proses Login/connection_database.php";
+        $namaUser = $_POST['nama_user'];
+        $usernameUser = $_POST['username_user'];
+        $emailUser = $_POST['email_user'];
+        $passwordUser = md5($_POST['password_user']);
+        $capt = md5($_POST['captcha_code']);
+        
+        $sql = "INSERT INTO users(username, nama_lengkap, email, password, captcha) VALUES ('$usernameUser', '$namaUser','$emailUser', '$passwordUser', '$capt')";
+        $query=mysqli_query($con, $sql);
+        mysqli_close($con);
+        header('location:Proses Login/login.php');
+    }
+
+    
+    
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+?>
     </head>
     <body>
         <section class="h-100 w-100" style="box-sizing: border-box; background-color: #232130">
@@ -41,7 +105,7 @@
             <h3 class="title-text">Register to continue</h3>
             <p class="caption-text">Please registered on the website.<br>
                 </p>
-            <form style="margin-top: 1.5rem;" action="check_register.php" method="post">
+            <form style="margin-top: 1.5rem;" action="registration.php" method="post">
             <div style="margin-bottom: 1.75rem;">   
                     <label for="exampleInputEmail1" class="d-block input-label">Fullname</label>
                     <div class="d-flex w-100 div-input">
@@ -52,9 +116,9 @@
                             fill="#4E4B62" />
                         </svg>
                         <input class="input-field" type="email_user" name="nama_user" id="exampleInputEmail1" placeholder="Your Fullname" autocomplete="on"
-                        required>
-                        <?php if(isset($_GET["errnama_user"])){$err = $_GET["errnama_user"]; echo $err;} ?>
+                        >
                     </div>
+                    <span class = "text-warning"><?php echo $namaErr;?></span>
                 </div>
                 <div style="margin-bottom: 1.75rem;">
                     <label for="exampleInputEmail1" class="d-block input-label">Username</label>
@@ -66,8 +130,9 @@
                             fill="#4E4B62" />
                         </svg>
                         <input class="input-field" type="email_user" name="username_user" id="exampleInputEmail1" placeholder="Your Username" autocomplete="on"
-                        required>
+                        >
                     </div>
+                    <span class = "text-warning"><?php echo $userErr;?></span>
                 </div>
                 <div style="margin-bottom: 1.75rem;">
                     <label for="exampleInputEmail1" class="d-block input-label">Email Address</label>
@@ -79,8 +144,9 @@
                             fill="#4E4B62" />
                         </svg>
                         <input class="input-field" type="email_user" name="email_user" id="exampleInputEmail1" placeholder="Your Email Address" autocomplete="on"
-                        required>
+                        >
                     </div>
+                    <span class = "text-warning"><?php echo $emailErr;?></span>
                 </div>
                 <div style="margin-top: 1rem;">
                     <label for="password-content-3-6" class="d-block input-label">Password</label>
@@ -92,7 +158,7 @@
                             fill="#4E4B62" />
                         </svg>
                         <input class="input-field" type="password" name="password_user" id="password-content-3-6" placeholder="Your Password"
-                        required>
+                        >
                         <div onclick="togglePassword()">
                         <svg style="margin-left: 0.75rem; cursor:pointer" width="20" height="14" viewBox="0 0 20 14"
                             fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -101,6 +167,7 @@
                             fill="#4E4B62" />
                         </svg>
                     </div>
+                    <span class = "text-warning"><?php echo $passErr;?></span>
                 </div>
                 <div style="margin-top: 1rem;">
                     <label for="password-content-3-6" class="d-block input-label">Confirm Password</label>
@@ -112,7 +179,7 @@
                             fill="#4E4B62" />
                         </svg>
                         <input class="input-field" type="password" name="password_user2" id="password-content-3-6" placeholder="Confirm Your Password"
-                        required>
+                        >
                         <div onclick="togglePassword()">
                         <svg style="margin-left: 0.75rem; cursor:pointer" width="20" height="14" viewBox="0 0 20 14"
                             fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -121,6 +188,7 @@
                             fill="#4E4B62" />
                         </svg>
                     </div>
+                    <span class = "text-warning"><?php echo $pass2Err;?></span>
                 </div>
                 <div style="margin-top: 1rem;">
                     <label for="captcha_code" class="d-block input-label">Captcha</label>
@@ -129,6 +197,7 @@
                         <input type="text" class="input-field" name="captcha_code" id="captcha_code"/>
                     </div>
                 </div>
+                <span class = "text-warning"><?php echo $captErr;?></span>
                 </div>
                 <div class="d-flex justify-content-end" style="margin-top: 0.75rem;">
                 <a href="#" class="forgot-password fst-italic">Forgot Password?</a>
@@ -146,6 +215,6 @@
         </div>
     </section>
         <!-- Option 1: Bootstrap Bundle with Popper -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></>
     </body>
 </html>
