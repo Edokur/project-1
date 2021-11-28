90abff0ced5b17688a263c21cb5b3ebdd3a2e8d9
Ajibon-cmd <teguhbayu52@gmail.com>
2021-11-28 15:39:15 +0700

    update login

--- Proses Login/login.php
+++ Proses Login/login.php
@@ -8,9 +8,7 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="../style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-<<<<<<< HEAD
         <?php
-<<<<<<< HEAD
 // define variables and set to empty values
 $emailErr = $passErr = $captErr = "";
 $email = $pass = $capt = "";
